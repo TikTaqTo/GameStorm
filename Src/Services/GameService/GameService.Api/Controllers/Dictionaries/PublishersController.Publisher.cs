4eeb7665e327ae50304bf8f3e075bdc73a58f5e2
TikTaqTo <adai.02_kz@mail.ru>
2021-10-04 00:16:21 +0600

    Adai-update-publisher

--- Src/Services/GameService/GameService.Api/Controllers/Dictionaries/PublishersController.Publisher.cs
+++ Src/Services/GameService/GameService.Api/Controllers/Dictionaries/PublishersController.Publisher.cs
@@ -4,6 +4,7 @@ using GameService.Application.Commands.Dictionaries.Genres.Create;
 using GameService.Application.Commands.Dictionaries.Genres.Delete;
 using GameService.Application.Commands.Dictionaries.Genres.Update;
 using GameService.Application.Commands.Dictionaries.Publishers.Create;
+using GameService.Application.Commands.Dictionaries.Publishers.Update;
 using GameService.Application.Queries.Dictionaries.Genres;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -33,5 +34,21 @@ namespace GameService.Api.Controllers.Dictionaries {
       return Ok(mappedCratedpublisherReply);
     }
 
+    /// <summary>
+    ///     Method to update publisher
+    /// </summary>
+    /// <param name="publisher-update"></param>
+    /// <returns></returns>
+    [HttpPost("update-publisher")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(PublisherReply))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(CommonReply))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(CommonReply))]
+    public async Task<IActionResult> UpdatePublisher([FromBody] Publisher publisher) {
+      var domainPublisher = _mapper.Map<Domain.EntityModels.Dictionaries.Publisher>(publisher);
+      var updatePublisherCommand = new UpdatePublisherCommand(domainPublisher);
+      var updatedPublisherReply = await _mediator.Send(updatePublisherCommand);
+      var mappedUpdatedPublisherReply = _mapper.Map<PublisherReply>(updatedPublisherReply);
+      return Ok(mappedUpdatedPublisherReply);
+    }
   }
 }

c66f0530398e38e983974aa5c05a0800ac81a145
TikTaqTo <adai.02_kz@mail.ru>
2021-10-04 00:10:49 +0600

    Adai-create-publisher

---
+++ Src/Services/GameService/GameService.Api/Controllers/Dictionaries/PublishersController.Publisher.cs
@@ -0,0 +1,37 @@
+﻿using GameService.Api.Model.Dictionaries;
+using GameService.Api.Model.Replies;
+using GameService.Application.Commands.Dictionaries.Genres.Create;
+using GameService.Application.Commands.Dictionaries.Genres.Delete;
+using GameService.Application.Commands.Dictionaries.Genres.Update;
+using GameService.Application.Commands.Dictionaries.Publishers.Create;
+using GameService.Application.Queries.Dictionaries.Genres;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GameService.Api.Controllers.Dictionaries {
+
+  public partial class PublishersController : ControllerBase {
+    #region Retrieve
+   
+    #endregion
+
+    /// <summary>
+    ///     Method to create publisher
+    /// </summary>
+    /// <param name="publisher-create"></param>
+    /// <returns></returns>
+    [HttpPost("create-publisher")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(PublisherReply))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(CommonReply))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(CommonReply))]
+    public async Task<IActionResult> CreatePublisher([FromBody] Publisher publisher) {
+      var domainPublisher = _mapper.Map<Domain.EntityModels.Dictionaries.Publisher>(publisher);
+      var createPublisherCommand = new CreatePublisherCommand(domainPublisher);
+      var cratedPublisherReply = await _mediator.Send(createPublisherCommand);
+      var mappedCratedpublisherReply = _mapper.Map<PublisherReply>(cratedPublisherReply);
+      return Ok(mappedCratedpublisherReply);
+    }
+
+  }
+}

7ef03c6653ff9bdc9812753deaf193ef32b89df7
TikTaqTo <adai.02_kz@mail.ru>
2021-10-04 00:21:13 +0600

    Adai-delete-publisher-by-id

--- Src/Services/GameService/GameService.Api/Controllers/Dictionaries/PublishersController.Publisher.cs
+++ Src/Services/GameService/GameService.Api/Controllers/Dictionaries/PublishersController.Publisher.cs
@@ -4,6 +4,7 @@ using GameService.Application.Commands.Dictionaries.Genres.Create;
 using GameService.Application.Commands.Dictionaries.Genres.Delete;
 using GameService.Application.Commands.Dictionaries.Genres.Update;
 using GameService.Application.Commands.Dictionaries.Publishers.Create;
+using GameService.Application.Commands.Dictionaries.Publishers.Delete;
 using GameService.Application.Queries.Dictionaries.Genres;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -33,5 +34,19 @@ namespace GameService.Api.Controllers.Dictionaries {
       return Ok(mappedCratedpublisherReply);
     }
 
+    /// <summary>
+    ///     Method to delete publisher by id
+    /// </summary>
+    /// <param name="publisher-delete"></param>
+    /// <returns></returns>
+    [HttpDelete("delete-publisher-by-id")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(PublisherReply))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(CommonReply))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(CommonReply))]
+    public async Task<IActionResult> DeletePublisher(int publisherId) {
+      var deletePublisherReply = await _mediator.Send(new DeletePublisherCommand(publisherId));
+      var mappedDeletedPublisherReply = _mapper.Map<PublisherReply>(deletePublisherReply);
+      return Ok(mappedDeletedPublisherReply);
+    }
   }
 }

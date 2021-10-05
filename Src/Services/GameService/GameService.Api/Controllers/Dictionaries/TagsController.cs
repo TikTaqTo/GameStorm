fb54ee54f78834af9803f03ee5495895b525bc26
TikTaqTo <adai.02_kz@mail.ru>
2021-10-05 19:00:24 +0600

    Added controller for adding tags

---
+++ Src/Services/GameService/GameService.Api/Controllers/Dictionaries/TagsController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameService.Api.Controllers.Dictionaries {
+  [ApiController]
+  [Route("api/genres")]
+  [Produces("application/json")]
+  public partial class TagsController : ControllerBase {
+    private readonly IMapper _mapper;
+    private readonly IMediator _mediator;
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="mediator"></param>
+    /// <param name="mapper"></param>
+    public TagsController(IMediator mediator, IMapper mapper) {
+      _mediator = mediator;
+      _mapper = mapper;
+    }
+  }
+}

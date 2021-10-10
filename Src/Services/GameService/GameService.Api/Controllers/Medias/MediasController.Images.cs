e2ee135d26756493c2f8c987b3f722383743039f
TikTaqTo <adai.02_kz@mail.ru>
2021-10-10 15:44:09 +0600

    Created a controller and a method for adding an image

---
+++ Src/Services/GameService/GameService.Api/Controllers/Medias/MediasController.Images.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using GameService.Api.Model.Media;
+using GameService.Api.Model.Replies;
+using GameService.Application.Commands.Media.Create;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameService.Api.Controllers.Medias {
+
+  public partial class MediasController {
+
+    [HttpPost("create-image")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ImageReply))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(CommonReply))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(CommonReply))]
+    public async Task<IActionResult> CreateImage([FromBody] Image image) {
+      var domainImage = _mapper.Map<Domain.EntityModels.Media.Image>(image);
+      var createCountryCommand = new CreateImageCommand(domainImage);
+      var cratedImageReply = await _mediator.Send(createCountryCommand);
+      var mappedCratedImageReply = _mapper.Map<ImageReply>(cratedImageReply);
+      return Ok(mappedCratedImageReply);
+    }
+  }
+}
---
+++ Src/Services/GameService/GameService.Api/Controllers/Medias/MediasController.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameService.Api.Controllers.Medias {
+
+  [ApiController]
+  [Route("api/medias")]
+  [Produces("application/json")]
+  public partial class MediasController : ControllerBase {
+    private readonly IMapper _mapper;
+    private readonly IMediator _mediator;
+
+    public MediasController(IMediator mediator, IMapper mapper) {
+      _mediator = mediator;
+      _mapper = mapper;
+    }
+  }
+}

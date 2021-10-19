a87723fa68e3b93a5c015f4f2c6b28c2e4925317
TikTaqTo <adai.02_kz@mail.ru>
2021-10-19 18:56:05 +0600

    Added mappers for all dictionaries classes

--- Src/Services/GameService/GameService.Api/MappingProfiles/DictionariesProfile.cs
+++ Src/Services/GameService/GameService.Api/MappingProfiles/DictionariesProfile.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using GameService.Api.Model.Dictionaries;
+using GameService.Api.Model.Media;
 using GameService.Api.Model.Replies;
+using GameService.Api.Model.VideoGame;
 
 namespace GameService.Api.MappingProfiles {
   public class DictionariesProfile : Profile {
@@ -8,18 +10,26 @@ namespace GameService.Api.MappingProfiles {
       CreateMap<CommonReply, Domain.Replies.CommonReply>().ReverseMap();
       CreateMap<DeveloperReply, Domain.Replies.DeveloperReply>().ReverseMap();
       CreateMap<DevelopersReply, Domain.Replies.DevelopersReply>().ReverseMap();
-      CreateMap<GameReply, Domain.Replies.GameReply>().ReverseMap();
-      CreateMap<GamesReply, Domain.Replies.GamesReply>().ReverseMap();
       CreateMap<GenreReply, Domain.Replies.GenreReply>().ReverseMap();
       CreateMap<GenresReply, Domain.Replies.GenresReply>().ReverseMap();
-      CreateMap<ImageReply, Domain.Replies.ImageReply>().ReverseMap();
-      CreateMap<ImagesReply, Domain.Replies.ImagesReply>().ReverseMap();
       CreateMap<PlatformReply, Domain.Replies.PlatformReply>().ReverseMap();
       CreateMap<PlatformsReply, Domain.Replies.PlatformsReply>().ReverseMap();
       CreateMap<PublisherReply, Domain.Replies.PublisherReply>().ReverseMap();
       CreateMap<PublishersReply, Domain.Replies.PublishersReply>().ReverseMap();
       CreateMap<TagReply, Domain.Replies.TagReply>().ReverseMap();
       CreateMap<TagsReply, Domain.Replies.TagsReply>().ReverseMap();
+      CreateMap<GameReply, Domain.Replies.GameReply>().ReverseMap();
+      CreateMap<GamesReply, Domain.Replies.GamesReply>().ReverseMap();
+      CreateMap<ImageReply, Domain.Replies.ImageReply>().ReverseMap();
+      CreateMap<ImagesReply, Domain.Replies.ImagesReply>().ReverseMap();
+
+      CreateMap<Developer, Domain.EntityModels.Dictionaries.Developer>().ReverseMap();
+      CreateMap<Genre, Domain.EntityModels.Dictionaries.Genre>().ReverseMap();
+      CreateMap<Platform, Domain.EntityModels.Dictionaries.Platform>().ReverseMap();
+      CreateMap<Publisher, Domain.EntityModels.Dictionaries.Publisher>().ReverseMap();
+      CreateMap<Tag, Domain.EntityModels.Dictionaries.Tag>().ReverseMap();
+      CreateMap<Image, Domain.EntityModels.Media.Image>().ReverseMap();
+      CreateMap<Game, Domain.EntityModels.VideoGame.Game>().ReverseMap();
     }
   }
 }

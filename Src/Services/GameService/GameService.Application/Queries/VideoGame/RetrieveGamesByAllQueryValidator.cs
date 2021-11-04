6c1482b5faeed33a56b8fcb519da365ef7d9112a
TikTaqTo <adai.02_kz@mail.ru>
2021-11-04 21:33:03 +0600

    Added validator RetrieveGamesByAllQueryValidator

---
+++ Src/Services/GameService/GameService.Application/Queries/VideoGame/RetrieveGamesByAllQueryValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameService.Application.Queries.VideoGame {
+
+  public class RetrieveGamesByAllQueryValidator : AbstractValidator<RetrieveGamesByAllQuery> {
+
+    public RetrieveGamesByAllQueryValidator() {
+      RuleFor(x => x.Page)
+        .NotEmpty()
+        .WithMessage("Page can not be empty");
+
+      RuleFor(x => x.PageSize)
+        .NotEmpty()
+        .WithMessage("Page Size can not be empty");
+    }
+  }
+}

e4987650d06e8a6f7a07841c97f170574642f9ba
TikTaqTo <adai.02_kz@mail.ru>
2021-12-16 22:23:25 +0600

    Added classes for mock testing

---
+++ Src/Services/GameService/GameService.Shared/Testing/Mock/MockedDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using Moq;
+
+namespace GameService.Shared.Testing.Mock {
+
+  public class MockedDbContext<TDbContext>
+    where TDbContext : DbContext {
+    private readonly Dictionary<Type, object> dbSets = new Dictionary<Type, object>();
+
+    public MockedDbContext(Mock<TDbContext> dbContextMock, params object[] dbSetMocks) {
+      this.DbContext = dbContextMock;
+
+      foreach (var dbSetMock in dbSetMocks) {
+        this.dbSets.Add(dbSetMock.GetType(), dbSetMock);
+      }
+    }
+
+    public Mock<TDbContext> DbContext { get; set; }
+
+    public Mock<DbSet<TEntity>> GetDbSet<TEntity>() where TEntity : class {
+      return this.dbSets[typeof(Mock<DbSet<TEntity>>)] as Mock<DbSet<TEntity>>;
+    }
+  }
+}

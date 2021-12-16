d2c0b035accfb746a4aca37505701f45bfe3de5a
TikTaqTo <adai.02_kz@mail.ru>
2021-12-16 21:29:05 +0600

    Added support for asynchronous operations for testing

---
+++ Src/Services/GameService/GameService.Shared/Testing/DbAsync/TestDbAsyncEnumerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Data.Entity.Infrastructure;
+using System.Threading;
+
+namespace GameService.Shared.Testing.DbAsync {
+
+  public class TestDbAsyncEnumerator<T> : IDbAsyncEnumerator<T> {
+    private readonly IEnumerator<T> inner;
+
+    public TestDbAsyncEnumerator(IEnumerator<T> inner) {
+      this.inner = inner;
+    }
+
+    public void Dispose() {
+      this.inner.Dispose();
+    }
+
+    public Task<bool> MoveNextAsync(CancellationToken cancellationToken) {
+      return Task.FromResult(this.inner.MoveNext());
+    }
+
+    public T Current {
+      get { return this.inner.Current; }
+    }
+
+    object IDbAsyncEnumerator.Current {
+      get { return this.Current; }
+    }
+  }
+}
